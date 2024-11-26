70342f280eef90e5eeb8f87da86936f98ace88b2
Louis Galloux <lgalloux@c1r3p1.42lehavre.fr>
2024-11-26 15:44:56 +0100

    test

--- get_next_line.c
+++ get_next_line.c
@@ -10,6 +10,26 @@ int		ft_strlen(char *str)
 	return (i);
 }
 
+int		ft_strlcat(char *src, char *dest, int size)
+{
+	int		i;
+	int		destlen;
+	int		space_left;
+
+	i = 0;	
+	destlen = ft_strlen(dest);
+	space_left = size - destlen - 1;
+	if (size < destlen)
+		return (0);
+	while (src[i] && i <= space_left)
+	{
+		dest[destlen + i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(dest));
+}
+
 char	*backjoin(char *str1, char *str2, t_back back)
 {
 	int	i;
@@ -31,7 +51,8 @@ char	*backjoin(char *str1, char *str2, t_back back)
 		result[i] = str1[i];
 		i++;
 	}
-	while (str2[j] && str2[j - 1] != '\n')
+	free(str1);
+	while (str2[j] && str2[j - 1] != eol)
 	{
 		result[i] = str2[j];
 		i++;
@@ -57,57 +78,42 @@ char *ft_strchr(char *str, char to_find)
 
 int		backcpy(char *src, char *dest, t_pos pos)
 {
-	if (pos == BEFORE)
-	{
-		while (str[i] && str[i - 1] != '\n')
-		{
-			result[i] = str[i];
-			i++;
-		}
-	}
-	else
-	{
-		str = ft_strchr(str, '\n');
-		if (!str)
-			return (NULL);
-		str++;
-		while (str[i])
-		{
-			result[i] = str[i];
-			i++;
-		}
-	}
-}
-
-char	*backdup(char *str, t_pos pos)
-{
-	char 	*result;
 	int		i;
 
 	i = 0;
 	if (pos == BEFORE)
 	{
-		result = malloc(ft_strlen(str) + 1);
-		while (str[i] && str[i - 1] != '\n')
+		while (src[i] && src[i - 1] != '\n')
 		{
-			result[i] = str[i];
+			dest[i] = src[i];
 			i++;
 		}
 	}
 	else
 	{
-		str = ft_strchr(str, '\n');
-		if (!str)
-			return (NULL);
-		str++;
-		result = malloc(ft_strlen(str) + 1);
-		while (str[i])
+		src = ft_strchr(src, '\n');
+		if (!src)
 		{
-			result[i] = str[i];
+			dest[0] = '\0';
+			return (0);
+		}
+		src++;
+		while (src[i])
+		{
+			dest[i] = src[i];
 			i++;
 		}
 	}
-	result[i] = '\0';
+	dest[i] = '\0';
+	return (ft_strlen(dest));
+}
+
+char	*backdup(char *str, t_pos pos)
+{
+	char 	*result;
+
+	result = malloc(ft_strlen(str) + 1);
+	backcpy(str, result, pos);
 	return (result);
 }
 
@@ -115,14 +121,15 @@ char *get_next_line(int fd)
 {
 	char		*clean_line;
 	char		dirty_line[BUFFER_SIZE] = {0};
-	static char *after_back;
+	static char after_back[BUFFER_SIZE] = {0};
 	static int	bytes_read = BUFFER_SIZE;
+	// char		*temp;
 
 	clean_line = NULL;
-	if (after_back)
+	if (after_back[0])
 		clean_line = backdup(after_back, BEFORE);
-	if (after_back && ft_strchr(after_back, '\n'))
-		after_back = backdup(after_back, AFTER);
+	if (ft_strchr(after_back, '\n'))
+		backcpy(after_back, after_back, AFTER);
 	while (!ft_strchr(dirty_line, '\n') && bytes_read)
 	{
 		if (clean_line && ft_strchr(clean_line, '\n'))
@@ -139,9 +146,9 @@ char *get_next_line(int fd)
 		else
 			clean_line = backjoin(clean_line, dirty_line, YES);
 	}
-	if (after_back && ft_strchr(after_back, '\n'))
-		after_back = backjoin(after_back, dirty_line, NO);
-	after_back = backdup(dirty_line, AFTER);
+	if (ft_strchr(after_back, '\n'))
+		ft_strlcat(dirty_line, after_back, BUFFER_SIZE);
+	backcpy(dirty_line, after_back, AFTER);
 	return (clean_line);
 }
 
@@ -150,11 +157,14 @@ int main(int argc, char **argv)
 	int	fd;
 	char *line;
 	(void)argc;
+	int		i;
 
+	i = 0;
 	fd = open(argv[1], O_RDONLY);
 	line = get_next_line(fd);
 	printf("%s", line);
-	while (line)
+	free(line);
+	while (line && i <= 15)
 	{
 		line = get_next_line(fd);
 		printf("%s", line);
